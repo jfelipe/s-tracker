874fbfa20bebc45d5c207a34b24c7a7e39505051
Juan Felipe Arango <jfelipe38@gmail.com>
2016-10-02 17:09:43 -0500

    Adding friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
   def my_portfolio
     @user = current_user
-    @user_stocks = @user.user_stocks
+    @user_stocks = current_user.user_stocks
+  end
+
+  def my_friends
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   root 'welcome#index'
 
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
